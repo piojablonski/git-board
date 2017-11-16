9ae714f1c19153a301cffb6e260962346731be1d
Piotr J <piojablonski@gmail.com>
2017-11-16 20:55:47 +0000

    added hash history

--- src/storeSetup.js
+++ src/storeSetup.js
@@ -1,10 +1,11 @@
 import { connectRoutes } from 'redux-first-router'
 import { combineReducers, createStore, applyMiddleware } from 'redux'
-import createHistory from 'history/createBrowserHistory'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { pageReducer } from './reducers/page.reducer'
+import { createHashHistory } from 'history'
 
-const history = createHistory()
+// we need hash to run project on git pages
+const history = createHashHistory()
 
 const routesMap = {
   ISSUES: '/issues',
